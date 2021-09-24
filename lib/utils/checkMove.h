0572809b28048dd7754315beadc7d31be3f7d064
izabellesebastiao <86986238+izabellesebastiao@users.noreply.github.com>
2021-09-24 18:37:23 -0300

    Update checkMove.h

--- lib/utils/checkMove.h
+++ lib/utils/checkMove.h
@@ -1,3 +1,27 @@
+/**
+ * ARQUIVO: lib/utils/checkMove.h
+ * 
+ * TÍTULO: RELATÓRIO DE TRABALHO DE LABORATÓRIO 2
+ * ENVIADO POR: Gustavo Simões e Izabelle Custodia Teixeira Sebastiao.            
+ *              Todo o código tem como autores Gustavo Simões e Izabelle C. T. Sebastião.
+ * 
+ * CURSO: Linguagem de progrmação 1 do curso de Enhenharia de Computação da UFSC
+ * DATA: 04/09/21 
+ * 
+ *
+ * PROPÓSITO:
+ * Este cabeçalho (.h) tem como objetivo verificar se o movimento do jogador é valido,
+ * ou seja, verifica se o movimento coincide com uma parede (*\|).
+ * 
+ * METÓDO GERAL:
+ * Verifica se o movimento de interesse do jogado coincide com uma parede (*) ou
+ * limite do mapa (|) e tem como retorno um número boleano.  
+ * 
+ * ARQUIVOS INCLUÍDOS:
+ * stdio.h
+ * tdbool.h
+ * global.h
+
 #ifndef CHECK_MOVE_H
 #define CHECK_MOVE_H
 
