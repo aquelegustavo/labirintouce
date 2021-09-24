a0af13e6482cb2467b72375c3e6ef02960d17d77
izabellesebastiao <86986238+izabellesebastiao@users.noreply.github.com>
2021-09-24 18:53:17 -0300

    Update invalidMove.h

--- lib/utils/invalidMove.h
+++ lib/utils/invalidMove.h
@@ -1,3 +1,25 @@
+/**
+ * ARQUIVO: lib/utils/invalidMove.h
+ * 
+ * TÍTULO: RELATÓRIO DE TRABALHO DE LABORATÓRIO 2
+ * ENVIADO POR: Gustavo Simões e Izabelle Custodia Teixeira Sebastiao.            
+ *              Todo o código tem como autores Gustavo Simões e Izabelle C. T. Sebastião.
+ * 
+ * CURSO: Linguagem de progrmação 1 do curso de Enhenharia de Computação da UFSC
+ * DATA: 04/09/21 
+ * 
+ * PROPÓSITO:
+ * Avisar ao usuario caso tenha obstaculo, ou seja, uma parede (*) ou um bloco(|).
+ * 
+ * METÓDO GERAL:
+ * Imprimir na tela uma mensagem de erro e incrementa em 1 a variável 
+ * global "invalidMovesCounter'
+ * 
+ * ARQUIVOS INCLUÍDOS:
+ * stdio.h
+ * global.h
+ * getRandom.h
+ */
 #ifndef INVALID_MOVE_H
 #define INVALID_MOVE_H
 
