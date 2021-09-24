0339112127f1a0471942a0216c404504e466021e
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2021-09-24 18:07:56 -0300

    Add documentation to lib/main.c
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- lib/main.c
+++ lib/main.c
@@ -1,3 +1,37 @@
+/**
+ * ARQUIVO: lib/main.c
+ * 
+ * TÍTULO: RELATÓRIO DE TRABALHO DE LABORATÓRIO 2
+ * ENVIADO POR: Gustavo Simões e Izabelle Custodia Teixeira Sebastiao.            
+ *              Todo o código tem como autores Gustavo Simões e Izabelle C. T. Sebastião.
+ * 
+ * CURSO: Linguagem de progrmação 1 do curso de Enhenharia de Computação da UFSC
+ * DATA: 04/09/21 
+ * 
+ *
+ * PROPÓSITO:
+ * Este trabalho tem como propósito criar um jogo do labirinto, em que o usuário, 
+ * por meio de comandos via teclado, controla um personagem que deve sair do 
+ * começo do labirinto até o final, encostado o mínimo de vezes possíveis nas
+ * paredes do mapa (*).
+ * 
+ * METÓDO GERAL:
+ * Criação dinâmica do mapa;
+ * Jogabilidade;
+ * Geração do relatório do jogo (saída de jogo);
+ * 
+ * ARQUIVOS INCLUÍDOS:
+ * stdio.h
+ * time.h
+ * stdlib.h
+ * global.h
+ * modules/printGameHeader.h
+ * modules/createMap.h
+ * modules/playerMove.h
+ * modules/endOutputFile.h
+ * 
+ */
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -42,7 +76,7 @@ int main()
         fprintf(outputFile, "Jogador(a) venceu o jogo!");
     }
 
-    // libera a memória da matriz
+    // libera a memória da matriz map
     for (size_t i = 0; i < rowsNumber; i++)
         free(map[i]);
     free(map);
